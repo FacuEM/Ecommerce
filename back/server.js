cf0dfec48ec7ff0ed9aa9fdc03e4c91826a10c50
Gonzalo Aguila <aguilagonzalo1@gmail.comgit commit -m testing>
2020-11-03 12:41:28 -0300

    config de passport completa

--- back/server.js
+++ back/server.js
@@ -1,9 +1,65 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
+const cookieParser = require('cookieParser')
+const passport = require("passport");
+const sessions = require("express-session");
 
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
+
+app.use(
+  sessions({
+    secret: "ecommerce",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+passport.use(
+  new localStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    function (email, password, done) {
+      User.findOne({ where: { email } })
+        .then((user) => {
+          if (!user) {
+            // email not found
+            return done(null, false);
+          }
+          user.hash(password, user.salt).then((hash) => {
+            if (hash !== user.password) {
+              return done(null, false); // wrong password
+            }
+
+            return done(null, user); // success :D
+          });
+        })
+        .catch(done); // done(err)
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findByPk(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch(done);
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public", "index.html"));
 });

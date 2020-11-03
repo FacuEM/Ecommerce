94e9ee7480cd012accbaf8bf10489983c802207a
WilliamSaya-lvl30 <williams.sand25@gmail.com>
2020-11-03 16:59:23 -0300

    relaciones de las tablas

--- back/api/routes/index.js
+++ back/api/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {User}=require('../../models')
 
-router.use("/user",);
-/* router.use("/products", require("./products")); */
+// router.use("/user",);
+// /* router.use("/products", require("./products")); */
 
 module.exports = router;
--- back/models/index.js
+++ back/models/index.js
@@ -4,6 +4,12 @@ const Category=require('./category')
 const Review=require('./review')
 const Orders=require('./orders')
 
+Orders.belongsTo(User)
+Orders.belongsToMany(Product,{ through: 'car' })
+Product.belongsToMany(Orders,{through:'car'})
+Review.belongsTo(User)
+Review.belongsTo(Product)
+
 module.exports={
     User,
     Product,

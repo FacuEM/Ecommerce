0f53fb7145b3702145ffa252596ed3cb6231ea0c
Lucas <lucasnovaroh@gmail.com>
2020-11-03 15:41:59 -0300

    esqueleto front listo

--- front/src/index.js
+++ front/src/index.js
@@ -2,12 +2,15 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
 const { Provider } = require("react-redux");
+import store from "../redux/store"
+import { BrowserRouter, Route } from "react-router-dom";
 import Main from "./containers/Main";
 
 ReactDOM.render(
-  <div>
-  <Main />
-  {console.log("Hola")}
-  </div>,
+  <Provider store={store}>
+    <BrowserRouter>
+    <Route path="/" component={Main} />
+  </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );

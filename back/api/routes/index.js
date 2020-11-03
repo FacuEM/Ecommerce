4b486bb0a4a09826af9bab80c81301fc59a91291
floyd <facundomartinezesc@gmail.com>
2020-11-03 18:52:02 -0300

    rutas categorias

--- back/api/routes/index.js
+++ back/api/routes/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const userRoutes = require("./user");
 const productsRoutes = require("./products");
+const categoryRoutes = require("./category");
 
 router.use("/user", userRoutes);
 router.use("/products", productsRoutes);
+router.use("/category", categoryRoutes);
 
 module.exports = router;

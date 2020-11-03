3633a86638496cf6a1b5c768ee8146671f20a75c
floyd <facundomartinezesc@gmail.com>
2020-11-03 12:41:33 -0300

    routes user 4/5

--- back/server.js
+++ back/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const path = require("path");
+const api = require("./api/routes");
 
 const app = express();
 
 app.use(express.static("public"));
+
+app.use("/api", api);
+
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public", "index.html"));
 });

f7e123a2c261febcee706feea1d7951682ae7133
Lucas <lucasnovaroh@gmail.com>
2020-11-04 17:31:40 -0300

    Login/Logut/Persistencia

--- front/src/containers/Main.jsx
+++ front/src/containers/Main.jsx
@@ -1,19 +1,44 @@
 import React from 'react'
 import { Route, Redirect, Switch } from 'react-router-dom';
+import {connect} from "react-redux"
 import Footer from '../components/Footer';
 import NavbarContainer from "../containers/NavbarContainer"
 import CategoriasContainer from "../containers/CategoriasContainer"
 import RegisterContainer from "../containers/RegisterContainer"
+import LoginContainer from "../containers/LoginContainer"
+import {fetchIsLogged} from "../../redux/actionCreators/loginUserCreator"
 
-export default function Main() {
+class Main extends React.Component {
+
+  componentDidMount() {
+    this.props.isLogged()
+  }
+
+  render(){
   return (
     <div>
-      <NavbarContainer component={NavbarContainer}/>
+      {/* <form>
+      <input id="email" placeholder="Mail"></input>
+        <input id="password" placeholder="Password" type="password"></input>
+        <button>Login</button>
+        <button> Logout </button>      
+        </form> */}
+        <NavbarContainer component={NavbarContainer}/>
       <Switch>
         <Route exact path="/" component={CategoriasContainer}/>
         <Route path="/register" component={RegisterContainer}/>
+        <Route path="/login" component={LoginContainer}/>
       </Switch>
       <Footer component={Footer}/>
     </div>
   )
 }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+   isLogged: (user) => dispatch(fetchIsLogged(user))
+ } 
+}
+
+export default connect(null, mapDispatchToProps)(Main);

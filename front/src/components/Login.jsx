b3f0c16bdd63d9ba7dc22acb2a86f6056fb0140c
floyd <facundomartinezesc@gmail.com>
2020-11-06 12:52:59 -0300

    before categories gonza

--- front/src/components/Login.jsx
+++ front/src/components/Login.jsx
@@ -1,17 +1,16 @@
 import React from "react"
-import { Link } from "react-router-dom"
 import {Form,Button}from"react-bootstrap"
 
 export default ({handleSubmit, handleChange, email, password}) => (
 <div className="form">
     <Form  onSubmit={handleSubmit}>
     
-    <Form.Group controlId="formBasicEmail">
+    <Form.Group >
         <Form.Label>Email</Form.Label>
         <Form.Control id="email" value={email} onChange={handleChange} type="email" placeholder="Enter email" />
     </Form.Group>
 
-    <Form.Group controlId="formBasicPassword">
+    <Form.Group >
         <Form.Label>Password</Form.Label>
         <Form.Control id="password" value={password} onChange={handleChange}type="password" placeholder="Password" />
     </Form.Group>
@@ -22,14 +21,3 @@ export default ({handleSubmit, handleChange, email, password}) => (
     </Form>
 </div>
 )
-// style = {{width:"5px", height:"auto", display:"inline-block"}}
-
-// <div>
-    // <form onSubmit={handleSubmit}>
-    //     <input id="email" value={email} placeholder="Mail" onChange={handleChange}></input><br/>
-    //     <input id="password" value={password} placeholder="Password" type="password" onChange={handleChange}></input><br/>
-    //     <button>Login</button>
-    //     <Link onClick={clickLogout} to="/">Logout</Link>
-    //     </form>
-    //   <br/>
-    // </div>

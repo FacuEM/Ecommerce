d14fd80f868da87c5fd1cad9ee5ca103547ba3dd
Gonzalo Aguila <aguilagonzalo1@gmail.comgit commit -m testing>
2020-11-11 15:41:15 -0300

    panel admin a medio hacer

--- front/redux/reducers/adminReducer.js
+++ front/redux/reducers/adminReducer.js
@@ -1,7 +1,8 @@
-import { FETCH_USERS } from "../constants";
+import { FETCH_USERS, FETCH_PRODUCTS, ADD_PRODUCT} from "../constants";
 
 const initialState = {
   users: [],
+  products: [],
 };
 
 export default (state = initialState, action) => {
@@ -9,7 +10,10 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case FETCH_USERS:
       return { ...state, users: action.data };
-
+    case FETCH_PRODUCTS:
+      return {...state, products: action.data}
+    case ADD_PRODUCT:
+      return {...state, products: [...state.products, action.data]}  
     default:
       return state;
   }
---
+++ front/src/components/Admin/AdminProducts.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {fetchAdminProducts, addProduct} from "../../../redux/actionCreators/adminCreator";
+import {Link} from 'react-router-dom'
+
+class AdminProducts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      descripcion: '',
+      price:  0,
+      stock: 0,
+      categoryId: 0,
+      image: ''
+    };
+    this.cambiarANum = this.cambiarANum.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+
+  cambiarANum(state){
+    return {...state, price: Number(state.price), stock: Number(state.stock), categoryId: Number(state.categoryId)}
+  }
+
+  handleSubmit(e){ 
+    e.preventDefault()
+    let nuevoProd = this.cambiarANum(this.state)
+    this.props.addProduct(nuevoProd)
+    this.setState({name: '',
+    descripcion: '',
+    price:  0,
+    stock: 0,
+    categoryId: 0,
+    image: ''})
+  }
+
+  handleChange(e){
+
+    this.setState({[e.target.name] : e.target.value})
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input onChange={this.handleChange} type="text" name='name' value={this.state.name} placeholder='Prod Name'/>
+          <input onChange={this.handleChange} type="text" name='image' value={this.state.image} placeholder='Prod Img Url'/>
+          <input onChange={this.handleChange} type="text" name='descripcion' value={this.state.descripcion} placeholder='Prod Description'/>
+          <input onChange={this.handleChange} type="text" name='price' value={this.state.price} placeholder='Prod Price (en pesos)'/>
+          <input onChange={this.handleChange} type="text" name='stock' value={this.state.stock} placeholder='Prod Stock'/>
+          <input onChange={this.handleChange} type="text" name='categoryId' value={this.state.categoryId} placeholder='Category ID'/>
+          <button type='submit'>Add Product</button>
+        </form>
+        <br/>
+        {this.props.products &&
+          this.props.products.map((product) => {
+            return (
+              <div key={product.id}>
+                <h3>product Id: {product.id}</h3>
+                <p>product name :{product.name}</p>
+                <p>product descript: {product.descripcion}</p>
+                <Link to={`/admin/products/update/${product.id}`}>Update</Link>
+                <button>Delete</button>
+              </div>
+            );
+          })}
+      </div>
+    );
+  }
+}
+
+const maptStateToProps = (state) => {
+  return {
+    products: state.admin.products
+  };  
+};
+
+export default connect(maptStateToProps, {fetchAdminProducts, addProduct})(AdminProducts);
